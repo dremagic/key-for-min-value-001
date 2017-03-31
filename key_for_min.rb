d848b9926ea05df809dc45cb5ad2c99f2cf65f5a
dremagic <deandre.ellison@gmail.com>
2017-03-31 11:12:43 -0500

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,4 +3,22 @@
 
 def key_for_min_value(name_hash)
 
+if name_hash.empty? then
+  lowest_key = nil
+else
+  base_value = 0
+  lowest_key = ""
+
+name_hash.each do |key_word,value_list|
+   if lowest_key == "" && base_value == 0 then
+    lowest_key = key_word
+    base_value = value_list
+   elsif value_list < base_value then
+    lowest_key = key_word
+    base_value = value_list
+  end
+
+end
+end
+lowest_key
 end
